f594b84bf80dbaf42caa81e18fb1be9b0f12fbf6
Bartosz Chojnacki <bartosz.chojnacki4@gmail.com>
2020-01-08 20:09:42 +0100

    initial

---
+++ Fetch Current Weather/weather.py
@@ -0,0 +1,12 @@
+"""Fetch Current Weather - Get the current weather for a given zip/postal code.
+Optional: Try locating the user automatically.  """
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://mylocation.org/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+a = soup.find("td", text="City").find_next_sibling("td").text
+print(a)
