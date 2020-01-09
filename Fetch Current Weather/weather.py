21486587edd72ad6ee3e5dde131893567335be25
Bartosz Chojnacki <Bartosz.Chojnacki@jmmj.pl>
2020-01-09 14:02:40 +0100

    Added Selenium stuff

--- Fetch Current Weather/weather.py
+++ Fetch Current Weather/weather.py
@@ -3,13 +3,20 @@ Optional: Try locating the user automatically.  """
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 URL = 'https://mylocation.org/'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-a = soup.find("td", text="City").find_next_sibling("td").text
-print(a)
+location = soup.find("td", text="City").find_next_sibling("td").text
 
-#
-# ee
+browser = webdriver.Chrome()
+browser.get('https://google.com/')
+searchElem = browser.find_element(By.NAME, 'q')
+query = 'pogoda ' + location
+searchElem.send_keys(query)
+searchElem.send_keys(Keys.ENTER)
